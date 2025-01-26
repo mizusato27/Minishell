f51f26ce1716e04d4af35b818222e04165105bca
Mizuki Sato <mizusato@c4r1s3.42tokyo.jp>
2025-01-26 18:54:32 +0900

    modified error.c

--- srcs/error.c
+++ srcs/error.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,7 +6,11 @@
 /*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 15:23:43 by ynihei            #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/01/26 18:56:59 by mizusato         ###   ########.fr       */
+=======
+/*   Updated: 2025/01/26 18:53:57 by mizusato         ###   ########.fr       */
+>>>>>>> f99f587 (modified error.c)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,72 +20,4 @@ void    malloc_error()
 {
     printf("malloc error\n");
     exit(1);
-=======
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include "minishell.h"
-
-#define ERROR_PREFIX "minishell: "
-
-bool	syntax_error = false;
-
-static void	perror_prefix(void)
-{
-	dprintf(STDERR_FILENO, "%s", ERROR_PREFIX);
-}
-
-void	fatal_error(const char *msg)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
-	exit(1);
-}
-
-void	assert_error(const char *msg)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "Assert Error: %s\n", msg);
-	exit(255);
-}
-
-void	err_exit(const char *location, const char *msg, int status)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "%s: %s\n", location, msg);
-	exit(status);
-}
-
-void	todo(const char *msg)
-{
-	perror_prefix();
-	dprintf(STDERR_FILENO, "TODO: %s\n", msg);
-	exit(255);
-}
-
-void	tokenize_error(const char *location, char **rest, char *line)
-{
-	syntax_error = true;
-	perror_prefix();
-	dprintf(STDERR_FILENO, "syntax error near unexpected character `%c' in %s\n", *line, location);
-	while (*line)
-		line++;
-	*rest = line;
-}
-
-void	parse_error(const char *location, t_token **rest, t_token *tok)
-{
-	syntax_error = true;
-	perror_prefix();
-	dprintf(STDERR_FILENO, "syntax error near unexpected token `%s' in %s\n", tok->word, location);
-	while (tok && !at_eof(tok))
-		tok = tok->next;
-	*rest = tok;
-}
-
-void	xperror(const char *location)
-{
-	perror_prefix();
-	perror(location);
->>>>>>> 79d7a5c (added ex)
 }
