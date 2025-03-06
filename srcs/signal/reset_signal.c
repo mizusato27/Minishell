8d4f00aba5a77c6e7b774c5854f3591b8f4e1b54
Yuta Nihei <ynihei@c4r3s17.42tokyo.jp>
2025-03-06 18:06:01 +0900

    signal 修正

---
+++ srcs/signal/reset_signal.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reset_signal.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynihei <ynihei@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 17:25:32 by ynihei            #+#    #+#             */
+/*   Updated: 2025/03/06 17:49:40 by ynihei           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+//シグナルの動作をデフォルトにリセットする
+void	reset_sig(int signum)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = SIG_DFL;
+	if (sigaction(signum, &sa, NULL) < 0)
+		fatal_error("sigaction");
+}
+
+//指定されたシグナルをデフォルトの動作にリセットする
+//SIGQUIT および SIGINT のシグナル動作をリセット
+//あまりいる理由が分からない
+void	reset_signal(void)
+{
+	reset_sig(SIGQUIT);
+	reset_sig(SIGINT);
+}
--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: ynihei <ynihei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 09:04:56 by ynihei            #+#    #+#             */
-/*   Updated: 2025/03/05 18:43:09 by ynihei           ###   ########.fr       */
+/*   Updated: 2025/03/06 18:04:53 by ynihei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,93 +19,51 @@ void	handler(int signum)
 	g_ctx.g_sig = signum;
 }
 
-/**
- * 指定したシグナルを無視する設定を行う
- */
-void	ignore_sig(int signum)
+//指定したシグナルを設定する（無視するか、指定されたハンドラを設定する）
+//SIG_IGNはシグナルを無視
+void	setup_signal_handlers(int signum, void (*handler)(int))
 {
 	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	sa.sa_handler = SIG_IGN; // シグナルを無視する
+	if (handler == SIG_IGN)
+		sa.sa_handler = SIG_IGN;
+	else
+		sa.sa_handler = handler;
 	if (sigaction(signum, &sa, NULL) < 0)
 		fatal_error("sigaction");
 }
 
-/**
- * SIGINT（Ctrl+C）のシグナルハンドラを設定する
- * シグナルを受信した際に handler 関数が呼ばれる
- */
-void	setup_sigint(void)
-{
-	struct sigaction	sa;
-
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = handler; // SIGINT を受けた際に handler を呼び出す
-	if (sigaction(SIGINT, &sa, NULL) < 0)
-		fatal_error("sigaction");
-}
-
-/**
- * シグナルの状態をチェックし、必要な処理を行う
- * @return 処理結果（常に 0 を返す）
- */
+// シグナルの状態をチェックし、必要な処理を行う
+// SIG_IGNは Ctr+C
+// rl_replace_line("", 0); // 入力行をクリア
+// rl_done = 1;            // readline ループを終了
+//g_ctx.g_rl_intr = true; 割り込み操作
 int	check_state(void)
 {
 	if (g_ctx.g_sig == 0)
 		return (0);
-	else if (g_ctx.g_sig == SIGINT) // Ctrl+C が押された場合
+	else if (g_ctx.g_sig == SIGINT)
 	{
 		g_ctx.g_sig = 0;
 		g_ctx.g_rl_intr = true;
-		rl_replace_line("", 0); // 入力行をクリア
-		rl_done = 1;            // readline ループを終了
+		rl_replace_line("", 0);
+		rl_done = 1;
 		return (0);
 	}
 	return (0);
 }
 
-/**
- * 必要なシグナル設定を行う
- * SIGQUIT を無視し、SIGINT を適切に処理する
- */
-//isattyはファイルディスクリプタが端末かどうかを判定する
+// 必要なシグナル設定を行う
+// SIGQUIT を無視し、SIGINT を適切に処理する
+// readline の出力先を標準エラー出力に設定
+// isattyはファイルディスクリプタが端末かどうかを判定する
 void	setup_signal(void)
 {
-	extern int	_rl_echo_control_chars;
-
-	_rl_echo_control_chars = 0; // Ctrl+文字 の表示を無効化
-	rl_outstream = stderr;      // readline の出力先を標準エラー出力に設定
+	rl_outstream = stderr;
 	if (isatty(STDIN_FILENO))
-		rl_event_hook = check_state; // シグナルチェックをイベントフックに設定
-	ignore_sig(SIGQUIT);             // SIGQUIT を無視する
-	setup_sigint();                  // SIGINT の設定
-}
-
-/**
- * シグナルの動作をデフォルトにリセットする
- * @param signum 対象のシグナル番号
- */
-void	reset_sig(int signum)
-{
-	struct sigaction	sa;
-
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = SIG_DFL; // デフォルトのシグナル動作
-	if (sigaction(signum, &sa, NULL) < 0)
-		fatal_error("sigaction");
-}
-
-/**
- * 指定されたシグナルをデフォルトの動作にリセットする
- * SIGQUIT および SIGINT のシグナル動作をリセット
- */
-//あまりいる理由が分からない
-void	reset_signal(void)
-{
-	reset_sig(SIGQUIT);
-	reset_sig(SIGINT);
+		rl_event_hook = check_state;
+	setup_signal_handlers(SIGQUIT, SIG_IGN);
+	setup_signal_handlers(SIGINT, handler);
 }
