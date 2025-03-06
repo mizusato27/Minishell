a522cb8e44337a448ff20c8a3a705f6afb216fa3
Mizuki Sato <mizusato@c4r3s15.42tokyo.jp>
2025-03-06 19:32:31 +0900

    parse heredoc修正箇所完了

--- srcs/parse/parse_redir.c
+++ srcs/parse/parse_redir.c
@@ -6,7 +6,7 @@
 /*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 20:05:30 by mizusato          #+#    #+#             */
-/*   Updated: 2025/03/05 18:55:29 by mizusato         ###   ########.fr       */
+/*   Updated: 2025/03/06 19:31:32 by mizusato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,21 @@ t_node	*parse_redir_append(t_token **rest, t_token *tok)
 	return (node);
 }
 
+static bool	check_quote(char *delim)
+{
+	if (ft_strchr(delim, SINGLE_QUOTE) == NULL
+		&& ft_strchr(delim, DOUBLE_QUOTE) == NULL)
+		return (false);
+	return (true);
+}
+
 t_node	*parse_redir_heredoc(t_token **rest, t_token *tok)
 {
 	t_node	*node;
 
 	node = new_node(ND_REDIR_HEREDOC);
 	node->delimiter = token_dup(tok->next);
-	node->is_delim_quoted = false;// <--- todo:ヒアドキュメントの展開の有無
+	node->is_delim_quoted = check_quote(node->delimiter->word);
 	node->targetfd = STDIN_FILENO;
 	*rest = tok->next->next;
 	return (node);
