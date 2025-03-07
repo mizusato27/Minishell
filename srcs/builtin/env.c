5810fb3b4801b27a6cabfb60f3cece3a2087ce52
佐藤瑞起 <117ema3cia@gmail.com>
2025-03-08 00:48:27 +0900

    env出力を逆に修正 / '_'の環境変数削除

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -3,26 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ynihei <ynihei@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mizusato <mizusato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 16:57:52 by ynihei            #+#    #+#             */
-/*   Updated: 2025/03/04 12:21:19 by ynihei           ###   ########.fr       */
+/*   Updated: 2025/03/08 00:45:12 by mizusato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	is_under(char c)
+{
+	if (c == '_')
+		return (true);
+	return (false);
+}
+
+static void	print_env(t_item *cursor)
+{
+	if (!cursor)
+		return ;
+	if (cursor->value)
+		print_env(cursor->next);
+	if (ft_strlen(cursor->name) == 1 && is_under(*(cursor->name)))
+		return ;
+	printf("%s=%s\n", cursor->name, cursor->value);
+}
 
 int	builtin_env(void)
 {
 	t_item	*cursor;
 
 	cursor = g_ctx.g_envmap->item_head.next;
-	while (cursor)
-	{
-		if (cursor->value)
-			printf("%s=%s\n", cursor->name, cursor->value);
-		cursor = cursor->next;
-	}
+	print_env(cursor);
 	printf("_=/usr/bin/env\n");
 	return (0);
 }
