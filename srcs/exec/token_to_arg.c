1bd32894f2bc59de2ca3d4e898705613e3953a01
yutao04 <yutakil0414@gmail.com>
2025-03-08 18:41:37 +0900

    token_to_argvのエラーを修正

--- srcs/exec/token_to_arg.c
+++ srcs/exec/token_to_arg.c
@@ -6,61 +6,123 @@
 /*   By: ynihei <ynihei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 19:17:38 by ynihei            #+#    #+#             */
-/*   Updated: 2025/03/08 18:26:23 by ynihei           ###   ########.fr       */
+/*   Updated: 2025/03/08 18:38:48 by ynihei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	*ft_realloc(void *ptr, size_t new_size)
+// static void	*ft_realloc(void *ptr, size_t new_size)
+// {
+// 	void	*new_ptr;
+
+// 	if (ptr == NULL)
+// 	{
+// 		new_ptr = malloc(new_size);
+// 		if (new_ptr == NULL)
+// 			malloc_error(ER_MALLOC);
+// 		return (new_ptr);
+// 	}
+// 	new_ptr = malloc(new_size);
+// 	if (new_ptr == NULL)
+// 		malloc_error(ER_MALLOC);
+// 	ft_memcpy(new_ptr, ptr, new_size);
+// 	free(ptr);
+// 	return (new_ptr);
+// }
+
+static void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
 	void	*new_ptr;
+	size_t	copy_size;
 
 	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			malloc_error(ER_MALLOC);
-		return (new_ptr);
-	}
+		return (malloc(new_size));
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		malloc_error(ER_MALLOC);
-	ft_memcpy(new_ptr, ptr, new_size);
+	if (old_size < new_size)
+		copy_size = old_size;
+	else
+		copy_size = new_size;
+	ft_memcpy(new_ptr, ptr, copy_size);
 	free(ptr);
 	return (new_ptr);
 }
 
-// 新しいサイズでメモリを再確保
-static char	**tail_recursive(t_token *tok, int nargs, char **argv)
+static char	**expand_argv(char **argv, int nargs)
 {
 	char	**new_argv;
+	size_t	old_size;
+	size_t	new_size;
 
-	if (tok == NULL || tok->kind == TK_EOF)
-		return (argv);
-	new_argv = ft_realloc(argv, (nargs + 2) * sizeof(char *));
+	old_size = (nargs + 1) * sizeof(char *);
+	new_size = (nargs + 2) * sizeof(char *);
+	new_argv = ft_realloc(argv, old_size, new_size);
 	if (new_argv == NULL)
 	{
-		free(argv);
+		free_argv(argv);
 		malloc_error(ER_REALLOC);
 	}
-	argv = new_argv;
-	argv[nargs] = ft_strdup(tok->word);
-	if (argv[nargs] == NULL)
-	{
-		free(argv);
-		malloc_error(ER_STRDUP);
-	}
-	argv[nargs + 1] = NULL;
-	return (tail_recursive(tok->next, nargs + 1, argv));
+	return (new_argv);
 }
 
 char	**token_list_to_argv(t_token *tok)
 {
+	int		nargs;
 	char	**argv;
 
-	argv = ft_calloc(1, sizeof(char *));
+	nargs = 0;
+	argv = malloc(sizeof(char *));
 	if (argv == NULL)
-		malloc_error(ER_CALLOC);
-	return (tail_recursive(tok, 0, argv));
+		malloc_error(ER_MALLOC);
+	argv[0] = NULL;
+	while (tok != NULL && tok->kind != TK_EOF)
+	{
+		argv = expand_argv(argv, nargs);
+		argv[nargs] = ft_strdup(tok->word);
+		if (argv[nargs] == NULL)
+		{
+			free_argv(argv);
+			malloc_error(ER_STRDUP);
+		}
+		nargs++;
+		argv[nargs] = NULL;
+		tok = tok->next;
+	}
+	return (argv);
 }
+
+// 新しいサイズでメモリを再確保
+// static char	**tail_recursive(t_token *tok, int nargs, char **argv)
+// {
+// 	char	**new_argv;
+
+// 	if (tok == NULL || tok->kind == TK_EOF)
+// 		return (argv);
+// 	new_argv = ft_realloc(argv, (nargs + 2) * sizeof(char *));
+// 	if (new_argv == NULL)
+// 	{
+// 		free(argv);
+// 		malloc_error(ER_REALLOC);
+// 	}
+// 	argv = new_argv;
+// 	argv[nargs] = ft_strdup(tok->word);
+// 	if (argv[nargs] == NULL)
+// 	{
+// 		free(argv);
+// 		malloc_error(ER_STRDUP);
+// 	}
+// 	argv[nargs + 1] = NULL;
+// 	return (tail_recursive(tok->next, nargs + 1, argv));
+// }
+
+// char	**token_list_to_argv(t_token *tok)
+// {
+// 	char	**argv;
+
+// 	argv = ft_calloc(1, sizeof(char *));
+// 	if (argv == NULL)
+// 		malloc_error(ER_CALLOC);
+// 	return (tail_recursive(tok, 0, argv));
+// }
